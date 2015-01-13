22a5e831630fdd62042f013c52c32468c095cf66
ivantchev <vasil.ivantchev@hotmail.com>
2015-01-13 23:18:52 +0200

    Intro-Programming-Homework

---
+++ 06_Problem_PrintNumbers/06_Problem_PrintNumbers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+    class PrintNumbers
+    {
+        static void Main()
+        {
+            int a = 1;
+            int b = 101;
+            int c = 1001;
+            Console.WriteLine("{0}, {1}, {2}",a,b,c);
+        }
+    }
+
---
+++ 07_Problem_PrintFirstAndLastName/07_Problem_PrintFirstAndLastName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    class PrintFirstAndLastName
+    {
+        static void Main()
+        {
+            string firstName = "Turanaga";
+            string lastName = "San";
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+        }
+    }
